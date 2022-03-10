90bfd7b63c0e6b183159f7667dff21fa1aa90146
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-03-10 14:59:21 +0300

    Added more js functionality to display the rolled dice

--- js/main.js
+++ js/main.js
@@ -6,19 +6,29 @@ const rollDiceTwo = document.getElementById('roll2');
 
 const getDice = function() {
       const showDice1 = document.querySelector('.dice-roll');
-      const showDice2 = document.getElementById('dice-roll-1');
-      let rolledDice = Math.trunc(Math.random() * 6) + 1;
-      const diceImage = "images/" + rolledDice + ".jpg";
-      if(rolledDice === 1) {
-            showDice1.classList.remove('hidden');
-            // image.setAttribute("images" "images/dice1.jpg");
-      }
-      console.log(`You rolled a ${rolledDice}`);
+      let rolledDice1 = Math.trunc(Math.random() * 6) + 1;
+      const diceImageSource = "images/dice-" + rolledDice1 + ".jpg";
+      let diceImage1 = document.querySelectorAll("img")[0];
+      
+      showDice1.classList.remove('hidden');
+      diceImage1.setAttribute("src", diceImageSource);
+      console.log(`You rolled a ${rolledDice1}`);
+}
+
+const getDice2 = function() {
+      const showDice2 = document.querySelector('.dice-roll-1');
+      let rolledDice2 = Math.trunc(Math.random() * 6) + 1;
+      const diceImageSource2 = "images/dice-" + rolledDice2 + ".jpg";
+      let diceImage2 = document.querySelectorAll("img")[1];
+      
+      showDice2.classList.remove('hidden');
+      diceImage2.setAttribute("src", diceImageSource2);
+      console.log(`You rolled a ${rolledDice2}`);
 }
 
 rollDiceOne.addEventListener('click', getDice);
-rollDiceTwo.addEventListener('click', getDice);
+rollDiceTwo.addEventListener('click', getDice2);
 
-function whoWins(){
-      rollDiceOne > rollDiceTwo ? console.log(`Player One Wins`) : console.log(`Player Two Wins`);
+if(rolledDice1 > rolledDice2){
+      document.querySelector('h2').innerHTML = "Player One Wins!"
 }
